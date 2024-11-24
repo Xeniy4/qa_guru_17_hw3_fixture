572e44d57ac4a0144a61849a0eaacf0a03811d8d
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2024-11-24 17:13:50 +0300

    Delete assert from requirements.txt

--- test_hw3.py
+++ test_hw3.py
@@ -5,18 +5,19 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope="session")
-def browser():
-    print("Браузер")
-    yield
-    print("Закрыть браузер")
-
-
-@pytest.fixture(scope="session")
 def prepare_browser():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+    browser.config.window_height = 900
+    browser.config.window_width = 1400
     browser.open('https://google.com')
     browser.config.timeout = 20
-    browser.config.window_height = 1000
-    browser.config.window_width = 500
+    yield
+    browser.quit()
+
+
+def test_browser(prepare_browser):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
+
