5f3f55f981bda38f02d798273d6eb0defb236421
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2024-11-24 17:15:07 +0300

    Delete assert from requirements.txt

--- conftest.py
+++ conftest.py
@@ -11,12 +11,3 @@ def browser():
     print("Закрыть браузер")
 
 
-@pytest.fixture(scope="session")
-def prepare_browser():
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.open('https://google.com')
-    browser.config.timeout = 20
-    browser.config.window_height = 1000
-    browser.config.window_width = 500
