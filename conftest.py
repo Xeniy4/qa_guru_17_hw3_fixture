db27502274a4c7baab2f7b8cd65c256eb8d650ca
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-02-26 20:49:14 +0300

    del test_fixtures and update conftest

--- conftest.py
+++ conftest.py
@@ -1,13 +1,18 @@
 
 import pytest
-from selene import browser, be, have
+from selene import browser
 from selenium import webdriver
 
-
 @pytest.fixture(scope="function")
-def browser():
-    print("Браузер")
+def prepare_browser():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+    browser.config.window_height = 900
+    browser.config.window_width = 1400
+    browser.config.timeout = 7
+    browser.open('https://duckduckgo.com/')
+
     yield
-    print("Закрыть браузер")
     browser.quit()
 
--- test_hw3.py
+++ test_hw3.py
@@ -1,29 +1,12 @@
-
-import pytest
 from selene import browser, be, have
-from selenium import webdriver
-
-
-@pytest.fixture(scope="session")
-def prepare_browser():
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-    browser.config.window_height = 900
-    browser.config.window_width = 1400
-    browser.config.timeout = 20
-    browser.open('https://google.com')
-
-    yield
-    browser.quit()
 
 
 def test_browser(prepare_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
-    browser.element('[name="q"]').clear()
+    browser.element("section[data-testid='mainline']").should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
+
 
 
 def test_something(prepare_browser):
-    browser.element('[name="q"]').type('111abc22s34').press_enter()
-    browser.element('[class="card-section"]').should(have.text('По запросу 111abc22s34 ничего не найдено.'))
+    browser.element('[name="q"]').type('adfa232323хв3eввтттт%#$%&').press_enter()
+    browser.element("section[data-testid='mainline']").should(have.text('По запросу adfa232323хв3eввтттт%#$%& результаты не найдены.'))
